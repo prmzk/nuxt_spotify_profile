c0f95363657bcfb029dd3e439d294babae17b92f
Adam Primarizki <adam.primarizki@gmail.com>
2023-10-09 16:12:28 +0700

    add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "nuxt-icon"],
   css: ["~/assets/css/main.css"],
   postcss: {
