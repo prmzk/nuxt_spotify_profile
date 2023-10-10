e16ad5ab07739d1f09087e9e4339a5e5b2162432
Adam Primarizki <adam.primarizki@gmail.com>
2023-10-11 00:36:21 +0700

    create privacy policy page

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-full min-h-[400px]">
+    <h1 class="text-white text-3xl text-center my-8 font-bold underline">
+      MyTastify Privacy Policy
+    </h1>
+    <p class="text-white text-md">
+      MyTastify is developed as an open source powered by Spotify Web API.
+    </p>
+    <p class="text-white text-md">
+      By using this app, you agree to the use of your sptofi account including:
+    </p>
+    <ul class="list-disc ml-8">
+      <li class="text-white">Username & email</li>
+      <li class="text-white">Top artist & tracks</li>
+      <li class="text-white">
+        Player status including: recently played tracks and playback state
+      </li>
+      <li class="text-white">Private and public library</li>
+    </ul>
+    <p class="text-white text-md">
+      None of your data that you shared is stored or collected anywhere on the
+      server, and not shared with any thirt party app/APIs. All data and
+      information is used solely to be displayed on this app.
+    </p>
+    <p class="text-white text-md">
+      Although you can rest assured that your data is not being stored or used
+      maliciously, if you would like to revoke Receiptify's permissions, you can
+      visit your apps page and click "REMOVE ACCESS" on Receiptify. Here is a
+      more detailed guide for doing so. But even with the statement above, if
+      you want to revoke this app's permission, you can manage it on
+      <a href="https://www.spotify.com/us/account/apps/">your spotify account</a
+      >. Detailed guide on how to do it
+      <a href="https://support.spotify.com/us/article/spotify-on-other-apps/"
+        >here</a
+      >.
+    </p>
+  </div>
+</template>
+
+<style>
+p,
+ul {
+  margin-bottom: 8px;
+}
+a {
+  text-decoration: underline;
+}
+</style>
